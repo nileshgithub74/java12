209542e548a07e43d5895fbc3f17f1a1210f04be
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-02-23 20:32:59 +0000

    override

---
+++ methodOverriding.java
@@ -0,0 +1,21 @@
+// class Super{
+
+//     public void display(){
+//         System.out.println("Super class Display");
+//     }
+// }
+
+// class Sub extends Super{
+//     public void display(){
+//         System.out.println("Sub class Display");
+//     }
+// }
+
+// public class methodOverriding{
+//     public static void main(String[] args){
+//         Super sup = new Super();
+//         sup.display();
+//         Sub sub = new Sub();
+//         sub.display();
+//     }
+// }
