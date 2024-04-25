70577bd959f60ce37bf5b3bb30c36c5b739c1b5d
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-04-25 12:31:52 +0000

    TreeMap

---
+++ TreeMapDemo.java
@@ -0,0 +1,20 @@
+// import java.util.*;
+// public class TreeMapDemo{
+//     public static void main(String[] args){
+
+//         // TreeMap<Integer,String> tm = new TreeMap<>(Map.of(0,"A",1,"B",2,"c",3,"D"));
+//         // tm.put(4,"E");
+//         // tm.put(5,"F");
+//         // System.out.println(tm.ceilingEntry(5).getValue());
+//         // System.out.println(tm);
+
+
+
+//         HashMap<Integer,String> tm = new HashMap<>(Map.of(0,"A",1,"B",2,"c",3,"D"));
+//         tm.put(4,"E");
+//         tm.put(5,"F");
+//         // System.out.println(tm.ceilingEntry(5).getValue());
+//         System.out.println(tm);
+
+//     }
+// }
