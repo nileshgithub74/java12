9bb46e62cc65a404adb3dd8211b2ea79dd63c876
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-04-26 21:28:45 +0000

    commit

--- DataStream.java
+++ DataStream.java
@@ -1,34 +1,34 @@
-import java.util.*;
+// // // // import java.util.*;
 
-import java.io.*;
+// // // // import java.io.*;
 
 
 
-public class DataStream {
-    class Student{
-        int rollno;
-        String name;
-        String dept;
-    }
+// // // // public class DataStream {
+// // // //     class Student{
+// // // //         int rollno;
+// // // //         String name;
+// // // //         String dept;
+// // // //     }
 
-    public static void main(String[] args)throws Exception
-    {
+// // // //     public static void main(String[] args)throws Exception
+// // // //     {
 
-        FileOutputStream fos =new FileOutputStream("file.txt");
-        DataOutputStream d = new DataOutputStream(fos);
+// // // //         FileOutputStream fos =new FileOutputStream("file.txt");
+// // // //         DataOutputStream d = new DataOutputStream(fos);
 
-        Student s = new Student() ;
-        s.rollno = 10;
-        s.name = "Nilesh";
-        s.dept= "Cse";
+// // // //         Student s = new Student() ;
+// // // //         s.rollno = 10;
+// // // //         s.name = "Nilesh";
+// // // //         s.dept= "Cse";
 
-        d.writeInt(s.rollno);
-        d.writeUTF(s.name);
-        d.writeUTF(s.dept);
+// // // //         d.writeInt(s.rollno);
+// // // //         d.writeUTF(s.name);
+// // // //         d.writeUTF(s.dept);
         
 
 
 
-    }
+// // // //     }
     
-}
+// // // // }
