1b2d1e4f6c8149ed5a45ba99a63b320c54a5ceac
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-04-26 19:19:17 +0000

    ReaderFIle

---
+++ fileReader.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class fileReader {
+    public static void main(String[] args)throws Exception
+     {
+    //     FileReader fr = new FileReader("file.txt");
+
+    //     int x;
+    //     while((x=fr.read() ) != -1){
+    //         System.out.print((char)x);
+    //     }
+    }
+    
+}
